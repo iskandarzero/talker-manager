bb7f373dca5bc36c82dd5d0c5766613724d39b21
iskandarzero <riderzerotg@gmail.com>
2022-08-04 03:40:38 -0300

    feat: add email and password validation

--- loginRouter.js
+++ loginRouter.js
@@ -5,14 +5,27 @@ const bodyParser = require('body-parser');
 const router = express.Router();
 router.use(bodyParser.json());
 
-const validateEmail = (req, _res, next) => {
+const validateEmail = (req, res, next) => {
   const { email } = req.body;
-  if (email) next();
+  const emailRegex = /^\w+(\[\+\.-\]?\w)*@\w+(\[\.-\]?\w+)*\.[a-z]+$/i;
+
+  if (!email) return res.status(400).json({ message: 'O campo "email" é obrigatório' });
+  if (!emailRegex.test(email)) {
+    return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
+  }
+
+  next();
 };
 
-const validatePassword = (req, _res, next) => {
+const validatePassword = (req, res, next) => {
   const { password } = req.body;
-  if (password) next();
+
+  if (!password) return res.status(400).json({ message: 'O campo "password" é obrigatório' });
+  if (password.length < 6) {
+    return res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
+  }
+  
+  next();
 };
 
 router.post('/', validateEmail, validatePassword, (_req, res) => {
