d9f5046dfa0baa85f710915855db4629adf0589a
iskandarzero <riderzerotg@gmail.com>
2022-08-04 17:10:54 -0300

    refactor: moved validation functions to auth-middleware.js

--- auth-middleware.js
+++ auth-middleware.js
@@ -7,4 +7,70 @@ const authorizationMiddleware = (req, res, next) => {
   next();
 };
 
-module.exports = authorizationMiddleware;
+const nameValidation = (req, res, next) => {
+  const { name } = req.body;
+
+  if (!name) return res.status(400).json({ message: 'O campo "name" é obrigatório' });
+  if (name.length < 3) {
+    return res.status(400).json({ message: 'O "name" deve ter pelo menos 3 caracteres' });
+  }
+
+  next();
+};
+
+const ageValidation = (req, res, next) => {
+  const { age } = req.body;
+
+  if (!age) return res.status(400).json({ message: 'O campo "age" é obrigatório' });
+  if (Number(age) < 18) {
+    return res.status(400).json({ message: 'A pessoa palestrante deve ser maior de idade' });
+  }
+
+  next();
+};
+
+const talkValidation = (req, res, next) => {
+  const { talk } = req.body;
+
+  if (!talk) return res.status(400).json({ message: 'O campo "talk" é obrigatório' });
+  const keys = Object.keys(talk);
+  if (!keys.includes('rate')) {
+    return res.status(400).json({ message: 'O campo "rate" é obrigatório' });
+  }
+  if (!keys.includes('watchedAt')) {
+    return res.status(400).json({ message: 'O campo "watchedAt" é obrigatório' });
+  }
+
+  next();
+};
+
+const dateValidation = (req, res, next) => {
+  const { talk } = req.body;
+  const regex = /^\d{2}\/\d{2}\/\d{4}$/;
+
+  if (!talk.watchedAt.match(regex)) {
+    return res.status(400).json({ message: 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"' });
+  }
+
+  next();
+};
+
+const rateValidation = (req, res, next) => {
+  const { talk } = req.body;
+  const { rate } = talk;
+
+  if (rate < 1 || rate > 5) {
+    return res.status(400).json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
+  }
+
+  next();
+};
+
+module.exports = {
+  authorizationMiddleware,
+  nameValidation,
+  ageValidation,
+  talkValidation,
+  dateValidation,
+  rateValidation,
+};
